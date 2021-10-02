4d412693bc9585e69b5f76d7e30cbf7dc72df680
Johny-Hinchliffe <82289369+Johny-Hinchliffe@users.noreply.github.com>
2021-10-02 16:25:45 +0100

    mad the overview page

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,8 +1,17 @@
-exports.getOverview = (req, res, next) => {
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utilities/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  //1) Get tour data
+  const tours = await Tour.find();
+
+  //2) build template
+  //3) render template from step 1
   res.status(200).render('overview', {
     title: 'All Tours',
+    tours,
   });
-};
+});
 
 exports.getTour = (req, res, next) => {
   res.status(200).render('tour', {
